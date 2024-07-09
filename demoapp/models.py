8a50ac5a53826e04d6bb8c544e4e4666fa9d3f5c
if3ll <sunnwydays@gmail.com>
2024-07-08 23:54:48 -0400

    unit tests

--- demoapp/models.py
+++ demoapp/models.py
@@ -41,7 +41,13 @@ class Menu(models.Model):
     def __str__(self) -> str:
         return self.name
 
+class Reservation(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    booking_time = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.first_name
 
 # migrating and adding an entry with models
 # Djangovenv\Scripts\Activate
--- demoapp/tests.py
+++ demoapp/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from datetime import datetime
+from django.urls import reverse
+from django.templatetags.static import static
+from .models import Menu, Reservation
 
 # Create your tests here.
+class MenuTestCase(TestCase):
+    def setUp(self):
+        Menu.objects.create(name="Mocha", price=5)
+        Menu.objects.create(name="Water", price=0)
+
+    def test_menu_items(self):
+        mocha = Menu.objects.get(name="Mocha")
+        water = Menu.objects.get(name="Water")
+        self.assertEqual(mocha.price, 5)
+        self.assertEqual(water.price, 0)
+
+    def test_menu_image(self):
+        response = self.client.get(reverse('menu'))
+        expected_url = static('img/dessert.jpg')
+        self.assertIn(expected_url.encode(), response.content)
+
+class ReservationTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.reservation = Reservation.objects.create(
+            first_name="John",
+            last_name="Randolph"
+        )
+    def test_fields(self):
+        self.assertIsInstance(self.reservation.first_name, str)
+        self.assertIsInstance(self.reservation.last_name, str)
